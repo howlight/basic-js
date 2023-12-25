9fa57e072a7c51069b300a4ab0c3e7cbb518328a
Aleksey <howlight19@gmail.com>
2023-12-25 19:30:43 +0300

    feat: complete some tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,7 +3,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,9 +14,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+    const cat = '^^';
+    let sumOfCats = 0;
+
+    // перебор матрицы во вложенном цикле
+    for (let i = 0; i < matrix.length; i++) {
+        let arr = matrix[i];
+        for (let j = 0; j < arr.length; j++) {
+            // если элемент matrix[i][j] равен cat,
+            // увеличиваем счетчик на 1
+            if (arr[j] === cat) {
+                sumOfCats += 1;
+            }
+        }
+    }
+    return sumOfCats;
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -2,22 +2,30 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+    if (Array.isArray(members)) {
+        let teamName = '';
+        members.forEach((element) => {
+            if (typeof element === 'string' || element instanceof String) {
+                teamName += element.trim()[0];
+            }
+        });
+        return teamName.toUpperCase().split('').sort().join('');
+    }
+    return false;
 }
 
 module.exports = {
-  createDreamTeam
+    createDreamTeam,
 };
